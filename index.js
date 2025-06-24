3f3e496fc1dfeba3dfddb57341ed9b3653798013
GokulRaj S <168752490+Gokulraj0906@users.noreply.github.com>
2025-06-24 20:30:46 +0530

    added the trust proxy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware setup
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
+app.set('trust proxy', 1);
 // CORS configuration for JWT-based API
 app.use(cors({
   origin: ['http://localhost:5173', 'http://192.168.1.7:3001'],
